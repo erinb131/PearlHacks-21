7cf054fafd40746b6bcd4bdedc51bebfaadda6d1
Erin Byrd <ebbyrd@email.unc.edu>
2021-02-20 17:52:54 -0500

    trying git with vscode buttons

--- CovidCarolina.py
+++ CovidCarolina.py
@@ -1,7 +1,5 @@
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
-#import Tkinter as tk     # python 2
-#import tkFont as tkfont  # python 2
 
 class SampleApp(tk.Tk):
 
