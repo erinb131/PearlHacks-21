e410d5ee9cd859c619ddcf9866b6a182f925d81f
Emma Lozevski <emlozevs@email.unc.edu>
2021-02-20 11:56:07 -0500

    Started our Grade Calculator!

---
+++ gradecalc.py
@@ -0,0 +1,11 @@
+"""Calculates grades based on inputs from the user."""
+
+__author__ = "Emma Lozevski"    # :D
+
+
+def main() -> None:
+    """Entrypoint to our program."""
+
+
+if __name__ == "__main__":
+    main()
