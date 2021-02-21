c8725e9951961b9baf2e279720aff4d92cabd37b
Emma Lozevski <emlozevs@email.unc.edu>
2021-02-21 08:45:02 -0500

    grade calculator now implemented in app :)

--- CovidCarolina.py
+++ CovidCarolina.py
@@ -1,11 +1,13 @@
 import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
+from tkinter import *
+from tkinter import ttk
 from winsound import *
 import pygame
 import webbrowser
-
 import datetime
 import time
+from typing import List, Dict
 
 def callback(url):
     webbrowser.open_new(url)
@@ -177,7 +179,126 @@ class GradeCalc(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Go back to the main page",
                            command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        button.pack(pady=5)
+                
+        def gradecalc_main() -> None:
+            """Calculates grades based on inputs from the user."""
+            categories: List[str] = [] 
+            all_grades: Dict[str, Dict[str, float]] = {} 
+            weights: Dict[str, int] = {}
+            num_assignments: Dict[str, int] = {}
+            assignment_lists: Dict[str, List[str]] = {}
+            category_avgs: Dict[str, float] = {}
+            weighted_avgs: List[float] = []
+
+            num_categories: int = int(input("# of categories in final grade breakdown: "))
+
+            for i in range(num_categories):
+                category: str = input("Name of Category: ")
+                categories.append(category)
+                weights[category] = int(input("Percent of Final Grade: ")) / 100
+                num_assignments[category] = int(input("Number of Assignments in Category: "))
+
+            for item in categories:
+                category_grades: Dict[str, float] = {}
+                category_assignments: List[str] = []
+                grade_list: List[float] = []
+
+                print(item + " Grades")
+
+                for j in range(num_assignments[item]):
+                    assignment: str = input("Assignment Name: ")
+                    category_assignments.append(assignment)
+                    grade: float = float(input("Grade: "))
+                    category_grades[assignment] = grade
+                    grade_list.append(grade)
+
+                all_grades[item] = category_grades
+                assignment_lists[item] = category_assignments
+                category_avgs[item] = sum(grade_list) / num_assignments[item]
+                weighted_avgs.append(category_avgs[item] * weights[item])
+
+            # current_grade: float = 0.0
+            final_grade: float = sum(weighted_avgs)
+            letter_grade: str = get_letter_grade(final_grade)
+
+            create_treeview(categories, category_avgs, weights, assignment_lists, all_grades)
+
+            grade_label = tk.Label(self, text=("Final Grade:    " + str(final_grade) + "    " + letter_grade), 
+                                    font=controller.title_font)
+            grade_label.pack(pady=5)
+
+        def get_letter_grade(num_grade: float) -> str:
+            """Takes a numerical grade as input and returns a letter grade
+            according to the UNC standard grading scale."""
+            letter: str = ""
+
+            if num_grade >= 93:
+                letter = "A"
+            elif num_grade >= 90:
+                letter = "A-"
+            elif num_grade >= 87:
+                letter = "B+"
+            elif num_grade >= 83:
+                letter = "B"
+            elif num_grade >= 80:
+                letter = "B-"
+            elif num_grade >= 77:
+                letter = "C+"
+            elif num_grade >= 73:
+                letter = "C"
+            elif num_grade >= 70:
+                letter = "C-"
+            elif num_grade >= 65:
+                letter = "D+"
+            elif num_grade >= 60:
+                letter = "D"
+            else:
+                letter = "F"
+
+            return letter
+
+        def create_treeview(headers: List[str], avg_grades: Dict[str, float], 
+                            weights_dict: Dict[str, int], children: Dict[str, List[str]],
+                            gradebook: Dict[str, Dict[str, float]]) -> None:
+            """Displays the input and calculated output from GradeCalc as a table."""
+            my_tree = ttk.Treeview(self)
+
+            # Define columns
+            my_tree["columns"] = ["Gradebook Item", "Grade", "Weight"]
+
+            # Format columns
+            my_tree.column("#0", width=120, minwidth=25)
+            my_tree.column("Gradebook Item", anchor=W, width=120)
+            my_tree.column("Grade", anchor=CENTER, width=80)
+            my_tree.column("Weight", anchor=W, width=120)
+
+            # Create headings
+            my_tree.heading("#0", text="Label", anchor=W)
+            my_tree.heading("Gradebook Item", text="Gradebook Item", anchor=W)
+            my_tree.heading("Grade", text="Grade", anchor=CENTER)
+            my_tree.heading("Weight", text="Weight", anchor=W)
+
+            # Add data
+            count: int = 0
+            count2: int = len(headers) + count
+
+            for k in headers:
+                my_tree.insert(parent="", index="end", iid=count, text="Category", 
+                                values=(k, str(avg_grades[k]), str(weights_dict[k])))
+
+                for child in children[k]:
+                    my_tree.insert(parent=count, index="end", iid=count2, 
+                                    text="Assignment", values=(child, gradebook[k][child], "-"))
+                    count2 += 1
+                    
+                count += 1
+
+            # Pack to the screen
+            my_tree.pack(pady=20)
+
+        button = tk.Button(self, text="Calculate Class Grades", command=gradecalc_main())
+        button.pack(pady=10)
 
 
 class MentalHealth(tk.Frame):
@@ -211,94 +332,10 @@ class CoursesZooms(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="Courses and Zoom Links", font=controller.title_font)
         label.pack()
-<<<<<<< HEAD
-
-        button = tk.Button(self, text="Go to the start page",
-=======
         button = tk.Button(self, text="Go back to the main page",
->>>>>>> 7240d0cc417ffff19a80600cda6c595875a71c01
                            command=lambda: controller.show_frame("StartPage"))
-        button.pack(pady=15)
+        button.pack()
 
-        def click():
-            added_class = my_class.get() + "\n" + my_days.get() + "\n" + my_time.get() + "\n" + my_zoom_link.get() + "\n" + my_zoom_pwd.get()
-            class_data.delete(0.0, tk.END)
-            new_class.append(added_class)
-            class_data.insert(tk.END, new_class)
-
-
-        class_name = tk.Label(self, text="Name of Class:", font=("Helvetica", 12), fg="royalblue", bg="lightblue")
-        class_name.pack()
-
-        my_class = tk.StringVar()
-        password_entry_box = tk.Entry(self, textvariable=my_class, font=("Helvetica", 12), width=30)
-        password_entry_box.focus_set()
-        password_entry_box.pack()
-
-        days = tk.Label(self, text="Days:", font=("Helvetica", 12), fg="royalblue", bg="lightblue")
-        days.pack()
-
-        my_days = tk.StringVar()
-        password_entry_box = tk.Entry(self, textvariable=my_days, font=("Helvetica", 12), width=30)
-        password_entry_box.focus_set()
-        password_entry_box.pack()
-
-        time = tk.Label(self, text="Time:", font=("Helvetica", 12), fg="royalblue", bg="lightblue")
-        time.pack()
-
-        my_time = tk.StringVar()
-        password_entry_box = tk.Entry(self, textvariable=my_time, font=("Helvetica", 12), width=30)
-        password_entry_box.focus_set()
-        password_entry_box.pack()
-
-        zoom_link = tk.Label(self, text="Zoom Link:", font=("Helvetica", 12), fg="royalblue", bg="lightblue")
-        zoom_link.pack()
-
-        my_zoom_link = tk.StringVar()
-        password_entry_box = tk.Entry(self, textvariable=my_zoom_link, font=("Helvetica", 12), width=30)
-        password_entry_box.focus_set()
-        password_entry_box.pack()
-
-        zoom_pwd = tk.Label(self, text="Zoom Password (type \"N/A\" if none):", font=("Helvetica", 12), fg="royalblue", bg="lightblue")
-        zoom_pwd.pack()
-
-        my_zoom_pwd = tk.StringVar()
-        password_entry_box = tk.Entry(self, textvariable=my_zoom_pwd, font=("Helvetica", 12), width=30)
-        password_entry_box.focus_set()
-        password_entry_box.pack()
-
-        submit_btn = tk.Button(self, text="Add Class", command=click)
-        submit_btn.pack(pady=15)
-
-        new_class: List[str] = []
-
-        space_label = tk.Label(self, height=6, bg="light blue")
-        space_label.pack(pady=10)
-
-
-        label = tk.Label(self, 
-                        text="Monday              Tuesday            Wednesday           Thursday              Friday", 
-                        font=controller.title_font, bg="lightblue")
-        label.pack()
-
-        space_label = tk.Label(self, height=6, bg="light blue")
-        space_label.pack(padx=150, side="left")
-
-        class_data = tk.Text(self, font=("Helvetica", 12), width=22, height=26)
-        class_data.pack(side = "left")
-
-        class_data2 = tk.Text(self, font=("Helvetica", 12), width=22, height=26)
-        class_data2.pack(side = "left")
-
-        class_data3 = tk.Text(self, font=("Helvetica", 12), width=22, height=26)
-        class_data3.pack(side = "left")
-
-        class_data4 = tk.Text(self, font=("Helvetica", 12), width=22, height=26)
-        class_data4.pack(side = "left")
-
-        class_data5 = tk.Text(self, font=("Helvetica", 12), width=22, height=26)
-        class_data5.pack(side = "left")
-    
 
 class Sounds(tk.Frame):
 
--- gradecalc.py
+++ gradecalc.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 import sys
 from csv import DictWriter
 from typing import List, Dict, Optional
+from tkinter import *
+from tkinter import ttk
 
 
 def main() -> None:
@@ -13,6 +15,7 @@ def main() -> None:
     all_grades: Dict[str, Dict[str, float]] = {}      # Grades
     weights: Dict[str, int] = {}        # Weights
     num_assignments: Dict[str, int] = {}
+    assignment_lists: Dict[str, List[str]] = {}
     category_avgs: Dict[str, float] = {}
     weighted_avgs: List[float] = []
 
@@ -26,17 +29,20 @@ def main() -> None:
 
     for item in categories:
         category_grades: Dict[str, float] = {}
+        category_assignments: List[str] = []
         grade_list: List[float] = []
 
         print(item + " Grades")
 
         for j in range(num_assignments[item]):
             assignment: str = input("Assignment Name: ")
+            category_assignments.append(assignment)
             grade: float = float(input("Grade: "))
             category_grades[assignment] = grade
             grade_list.append(grade)
 
         all_grades[item] = category_grades
+        assignment_lists[item] = category_assignments
         category_avgs[item] = sum(grade_list) / num_assignments[item]
         weighted_avgs.append(category_avgs[item] * weights[item])
 
@@ -47,11 +53,14 @@ def main() -> None:
     # print(categories)
     # print(weights)
     # print(num_assignments)
+    print(category_assignments)
     
     print(all_grades)
     print(category_avgs)
     print(weighted_avgs)
     print(final_grade, letter_grade)
+
+    create_treeview(categories, category_avgs, weights, assignment_lists, all_grades)
 
 
 def get_letter_grade(num_grade: float) -> str:
@@ -83,6 +92,50 @@ def get_letter_grade(num_grade: float) -> str:
         letter = "F"
 
     return letter
+
+
+def create_treeview(headers: List[str], avg_grades: Dict[str, float], 
+                    weights_dict: Dict[str, int], children: Dict[str, List[str]],
+                    gradebook: Dict[str, Dict[str, float]]) -> None:
+    """Creates an app that displays the grade data in a table."""
+    root = Tk()
+    root.title("app name")
+    root.geometry("500x500")
+
+    my_tree = ttk.Treeview(root)
+
+    # Define columns
+    my_tree["columns"] = ["Gradebook Item", "Grade", "Weight"]
+
+    # Format columns
+    my_tree.column("#0", width=120, minwidth=25)
+    my_tree.column("Gradebook Item", anchor=W, width=120)
+    my_tree.column("Grade", anchor=CENTER, width=80)
+    my_tree.column("Weight", anchor=W, width=120)
+
+    # Create headings
+    my_tree.heading("#0", text="Label", anchor=W)
+    my_tree.heading("Gradebook Item", text="Gradebook Item", anchor=W)
+    my_tree.heading("Grade", text="Grade", anchor=CENTER)
+    my_tree.heading("Weight", text="Weight", anchor=W)
+
+    # Add data
+    count: int = 0
+
+    for k in headers:
+        my_tree.insert(parent="", index="end", iid=count, text="Parent", 
+                        values=(k, str(avg_grades[k]), str(weights_dict[k])))
+        
+        for child in children[k]:
+            my_tree.insert(parent=count, index="end", iid=(len(headers) + count), 
+                            text="Child", values=(child, gradebook[k][child], "-"))
+
+        count += 1
+
+    # Pack to the screen
+    my_tree.pack(pady=20)
+
+    root.mainloop()
 
 
 # def write_grade_table() -> None:
